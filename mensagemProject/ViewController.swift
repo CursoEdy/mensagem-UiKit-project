d7e6943e2dcf35295819cbb1937b3b8d8df4ca50
edy alves <30.edyalves@gmail.com>
2024-12-21 18:55:28 -0300

    Initial Commit

---
+++ mensagemProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mensagemProject
+//
+//  Created by ednardo alves on 21/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
